4754cadbeff3f4dd0c64fa386dcef75ec10ac0b5
Mohammad Zafar <mzcoder7@gmail.com>
2023-04-15 13:27:28 +0100

    task 1 - blog post

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,14 @@
-function BlogPost() {
-  return null;
+// Create a component BlogPost (for this exercise, make it the default export in src/components/BlogPost/index.js)
+
+function BlogPost({ blog }) {
+	return (
+		<div className='blog-post'>
+			<h2 className='blog-title'>{blog.title}</h2>
+			<p className='blog-author'>{blog.author}</p>
+			<p className='blog-date'>{blog.datePosted}</p>
+			<img src={blog.imageSrc} alt={blog.imageAlt} className='blog-image' />
+		</div>
+	);
 }
 
 export default BlogPost;
